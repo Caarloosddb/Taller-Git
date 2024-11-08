49ab9224c8a27d9ea99b4baa6e9073051312fbcf
Caarloosddb <carlosdediego2005@gmail.com>
2024-11-08 13:56:17 +0100

    Agregar archivo contador

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,21 @@
 <script>
-  let count = $state(0)
-  const increment = () => {
-    count += 1
-  }
-</script>
 
-<button onclick={increment}>
-  count is {count}
-</button>
+  let count = 0
+  
+  const increment = () => {
+  
+  if(count<20){
+    count +=1
+  }
+  }
+  const decrement = () => {
+    if(count>0){
+      count -=1
+    }
+  }
+  
+  </script>
+   <h1>{count}</h1>
+   <button id="+" onclick="{increment}">+</button>
+   <button id="-" onclick="{decrement}">-</button>
+  

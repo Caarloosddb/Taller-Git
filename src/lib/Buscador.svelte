01cc37b95dace6946dc1428ab8ea7773d98ac891
AlbertoAldana <alberto.ald.ort@gmail.com>
2024-11-15 12:51:44 +0100

    Add Buscador

---
+++ src/lib/Buscador.svelte
@@ -0,0 +1,25 @@
+<script>
+
+let search = ""; 
+const items = ['manzana', 'potato', 'martin', 'naranja'];
+
+function filterItems(){
+    return items.filter(item=>
+        item.toLowerCase().includes(search.toLowerCase())
+    );
+}
+</script>
+
+<p>BUSCADOR</p>
+<div class="container">
+<input type="text" bind:value={search}>
+
+
+
+<ul>
+    {#each filterItems() as item}
+    <li>{item}</li>
+    {/each}
+</ul>
+
+</div>
